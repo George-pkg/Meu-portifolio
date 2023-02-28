6a3b41212408a8058575c323fe818566d7b38baf
Gerge <67079118+kawazuke@users.noreply.github.com>
2023-02-28 11:25:17 -0300

    upgrades

---
+++ js/index.js
@@ -0,0 +1,21 @@
+var el = document.querySelector("#animaçao_nomes");
+var animaçao_nomes = "eorge Guiro|"
+var interval = 200;
+function showtext(el, animaçao_nomes, interval) {
+  var char = animaçao_nomes.split("").reverse();
+  
+  var typer = setInterval(function() {
+  
+    if (!char.length) {
+        return clearInterval(typer)
+    }
+    
+    var next = char.pop();
+    
+    el.innerHTML += next;
+    
+  }, interval);
+  
+}
+showtext(el, animaçao_nomes, interval);
+
